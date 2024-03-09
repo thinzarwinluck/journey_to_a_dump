c4c05ddea5f82c6583bd48d810f3cb5d874d9238
thinzarwinluck <thinzarwinluck0909@gmail.com>
2024-03-09 12:09:28 +0630

    build : tailwind

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+              </h1>
+        </nav>
+    </div>
+</template>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
